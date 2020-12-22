8c39e20c3733f0089fb06e93a56c2214bc10dbe2
sarkah01 <sarkah01@BFS-CA645327-N.dpi.nsw.gov.au>
2020-12-22 16:05:07 +1100

    Updated folder structure.

---
+++ sampleTest/src/test/java/com/au/byteUX/Page/Package/UX_BKR_Lic_Renewal.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package com.au.byteUX.Page.Package;
+
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+/**
+ * @author sarkah01
+ *
+ */
+public class UX_BKR_Lic_Renewal {
+
+	WebDriver driver;
+	
+	@FindBy(how = How.XPATH, using = "//div[text()='Renew my Authorisation']")	@CacheLookup WebElement renewMyAuthorisation;
+	@FindBy(how = How.XPATH, using = "//div[4]/.//div[2]/div[1]/div[3]/.//td[2]/div")	@CacheLookup WebElement totalNoOfHives;
+	@FindBy(how = How.XPATH, using = "//div[7]/.//tr[2]/td[2]/.//input")	@CacheLookup WebElement changeClassNO;
+	@FindBy(how = How.XPATH, using = "//div[7]/.//tr[2]/td[3]/.//input")	@CacheLookup WebElement changeClassYES;
+	@FindBy(how = How.NAME, using = "ConsentFinal")	@CacheLookup WebElement consent;
+	@FindBy(how = How.XPATH, using = "//span[text()='Submit my renewal application']")	@CacheLookup WebElement submitBtn;
+	@FindBy(how = How.XPATH, using = "//span[text()='Cancel']")	@CacheLookup WebElement cancelBtn;
+	@FindBy(how = How.XPATH, using = "//span[text()='OK']")	@CacheLookup WebElement clickOKToOpenPaymentForm;
+	@FindBy(how = How.XPATH, using = "//div[contains(text(),'Payment amount:')]")	@CacheLookup WebElement getAmount;
+	@FindBy(how = How.XPATH, using = "//div[2]/span") @CacheLookup WebElement getTrackingCode;
+	@FindBy(how = How.ID, using = "cardholderName") @CacheLookup WebElement cardHolderName;
+	//@FindBy(how = How.XPATH, using = "//*[@id=\"cardholderNameField\"]/label") @CacheLookup WebElement cardHolderName;
+	@FindBy(how = How.NAME, using = "cardNumber") @CacheLookup WebElement cardNumber;
+	@FindBy(how = How.NAME, using = "expiryDateMonth") @CacheLookup WebElement expiryDateMonth;
+	@FindBy(how = How.XPATH, using = "//html/body/div/form/div[3]/div/div[1]/select/option[5]") @CacheLookup WebElement selectMonth;
+	@FindBy(how = How.NAME, using = "expiryDateYear") @CacheLookup WebElement expiryDateYear;
+	@FindBy(how = How.XPATH, using = "//html/body/div/form/div[3]/div/div[2]/select/option[6]") @CacheLookup WebElement selectYear;
+	@FindBy(how = How.NAME, using = "cvn") @CacheLookup WebElement cvnNumber;
+	// If condition required - Cancel on UX "Do you wish to renew other authorisation that is due for renewals.": //a[4]/span/span/span[text()='Cancel']
+			
+	public UX_BKR_Lic_Renewal(WebDriver driver)  //constructor
+	{
+		this.driver = driver;
+	}
+	
+	public void renewMyAuth()
+	{
+		renewMyAuthorisation.click();
+		System.out.println("test clcik RMA");
+	}
+		
+	public void submitRenewal_NoPermissionChange ()
+	{
+		try {
+			int totalHives = Integer.parseInt(totalNoOfHives.getText());
+			
+			if (totalHives >= Integer.parseInt("50"))
+			{
+				totalNoOfHives.clear();
+				totalNoOfHives.sendKeys("40");
+			}
+			else
+			{
+				changeClassNO.click();
+				consent.click();
+				submitBtn.click();
+				//driver.switchTo().window("${win202}");
+				
+			}
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e)		{			e.getMessage();		}		
+	}
+	
+	public void submitPayment()
+	{
+		try
+		{
+			String parentHandle = driver.getWindowHandle(); // get the current window handle
+				System.out.println(parentHandle);
+			clickOKToOpenPaymentForm.click(); // click some link that opens a new window
+			Thread.sleep(1000);
+			for (String winHandle : driver.getWindowHandles()) {
+			    driver.switchTo().window(winHandle); // switch focus of WebDriver to the next found window handle (that's your newly opened window)
+			    	System.out.println(driver.getWindowHandle());
+			}
+			Thread.sleep(5000);
+			//code to do something on new window
+			System.out.println("Please verify the amount for Beekeeper Recreational: " + getAmount.getText());
+			System.out.println("Please verify the tracking code submission in Smart Client: " + getTrackingCode.getText());
+			//cardHolderName.getText();
+			System.out.println("cardHolderName is displayed" + cardHolderName.isDisplayed());
+			//cardHolderName.sendKeys("Jack");
+			//cardNumber.sendKeys("4444333322221111");
+			//expiryDateMonth.click();
+			//selectMonth.click();
+			//expiryDateYear.click();
+			//selectYear.click();
+			//cvnNumber.sendKeys("234");
+
+			//driver.close(); // close newly opened window when done with it
+			//driver.switchTo().window(parentHandle); // switch back to the original window
+		}
+		catch (InterruptedException e)		{			e.getMessage();		}
+	}
+
+}
+
---
+++ sampleTest/src/test/java/com/au/byteUX/Test/Package/UX_SubmitPayment_Bee_Renewal.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.au.byteUX.Test.Package;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.au.byteUX.Package.LocalDriverManager;
+import com.au.byteUX.Page.Package.LoginPage;
+import com.au.byteUX.Page.Package.MyAuthorisation;
+import com.au.byteUX.Page.Package.SelectSubject;
+import com.au.byteUX.Page.Package.UX_BKR_Lic_Renewal;
+
+import lib.ReadProperties;
+
+/**
+ * @author sarkah01
+ *
+ */
+public class UX_SubmitPayment_Bee_Renewal {
+WebDriver driver;
+	
+		
+	//Verify Renewal form submission
+	
+	@Test	
+	public void verifyRenewal() throws InterruptedException, FileNotFoundException, IOException
+	{
+		WebDriver driver = LocalDriverManager.getDriver();
+		String url = ReadProperties.getObject("config","UX");
+		String username = ReadProperties.getObject("config","external_User_Deb_Syd");
+		String password = ReadProperties.getObject("config","external_pwd");
+		driver.get(url);
+		System.out.println("Test1");
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		loginPage.LoginToUX(username, password);
+		Thread.sleep(5000);
+		
+		UX_BKR_Lic_Renewal RenewPage = PageFactory.initElements(driver, UX_BKR_Lic_Renewal.class);
+		Thread.sleep(5000);
+		
+		RenewPage.renewMyAuth();
+		System.out.println("Clicked on Renew My Authorisation");
+		Thread.sleep(10000);
+		
+		MyAuthorisation myAuth = PageFactory.initElements(driver, MyAuthorisation.class);
+		myAuth.multipleAuthorisation_select_Bee();
+		Thread.sleep(5000);
+		
+		RenewPage.submitRenewal_NoPermissionChange();
+		
+		RenewPage.submitPayment();
+		Thread.sleep(5000);
+	}
+
+}
